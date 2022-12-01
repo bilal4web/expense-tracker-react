0e3e0f3a04d406d52a191d884c7a66ba9f948b1e
Bilal Ahmad <bilal.dourani@outlook.com>
2022-12-01 22:03:36 +0500

    Added to github

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div>
+      <h2>Expense Tracker <sub><em style={{'font-weight':'normal','font-size':'8px',}}><pre>by Bilal Ahmad Durrani</pre></em></sub></h2>
+    </div>
+      )
+}
